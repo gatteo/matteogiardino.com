a50fc428121b4e4ddb6a0de546f01e3ddf51b176
Matteo Giardino <mat.gia.dev@gmail.com>
2023-06-10 18:11:50 +0200

    wip: spotify api logs

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -42,6 +42,10 @@ const getAccessToken = async () => {
         }),
     })
 
+    console.log('Spotify access token data', { REFRESH_TOKEN, BASIC })
+    console.log('Spotify access token response status', response.status)
+    console.log('Spotify access token response', await response.json())
+
     return (await response.json()) as AccessToken
 }
 
