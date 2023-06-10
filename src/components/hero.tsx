7c093f685e941142ac3197cc43f72373f0143f4a
Matteo Giardino <mat.gia.dev@gmail.com>
2023-06-10 12:47:50 +0200

    style: fix mobile hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -12,12 +12,12 @@ const Hero = () => {
                 <div className="flex flex-col-reverse gap-8 pt-24 md:flex-row md:justify-between md:pt-0">
                     <div className="md:max-w-lg">
                         {/* Name */}
-                        <h1 className="animate-fade-in text-5xl font-bold">Matteo Giardino</h1>
+                        <h1 className="animate-fade-in text-4xl font-bold sm:text-5xl">Matteo Giardino</h1>
 
                         {/* Occupation and description */}
                         <div className="my-6 space-y-2">
-                            <h2 className="space-x-2 font-medium leading-8">
-                                Founder & CTO @{' '}
+                            <h2 className="flex flex-wrap gap-x-2 text-sm font-medium leading-8 md:text-base">
+                                <span>Founder & CTO @ </span>
                                 <a
                                     href="https://westudents.it"
                                     className="inline-block transition-colors hover:text-sky-400">
@@ -26,7 +26,7 @@ const Hero = () => {
                                         alt="Westudents logo"
                                         width={24}
                                         height={24}
-                                        className="-mt-1 mr-2 inline-block h-6 rounded border border-accent-3"
+                                        className="-mt-1 mr-1 inline-block h-6 rounded border border-accent-3"
                                     />
                                     Westudents{' '}
                                 </a>
@@ -36,7 +36,7 @@ const Hero = () => {
                                         alt="Devv logo"
                                         width={24}
                                         height={24}
-                                        className="-mt-1 mr-2 inline-block h-6 rounded border border-accent-3"
+                                        className="-mt-1 mr-1 inline-block h-6 rounded border border-accent-3"
                                     />
                                     Devv{' '}
                                 </a>
@@ -48,14 +48,14 @@ const Hero = () => {
                                         alt="Devv logo"
                                         width={24}
                                         height={24}
-                                        className="-mt-1 mr-2 inline-block h-6 rounded border border-accent-3"
+                                        className="-mt-1 mr-1 inline-block h-6 rounded border border-accent-3"
                                     />
                                     Wezard{' '}
                                 </a>
                             </h2>
 
-                            <p className="text-lg text-accent-5">
-                                Un programmatore innamorato della tecnologia, un imprenditore innamorato delle idee e un
+                            <p className=" text-accent-5 md:text-lg">
+                                Un programmatore innamorato della tecnologia, imprenditore innamorato delle idee ed
                                 educatore innamorato della condivisione.
                             </p>
                         </div>
@@ -102,7 +102,7 @@ const Hero = () => {
                     ))}
                 </div> */}
             </div>
-            <div className="absolute bottom-0 left-0 right-0 top-0 max-h-screen w-screen">
+            <div className="absolute bottom-0 left-0 right-0 top-0 -z-50 max-h-screen w-screen">
                 {/* Scroll down */}
                 <div className="absolute bottom-0 left-0 right-0 mb-4">
                     <button
@@ -117,7 +117,7 @@ const Hero = () => {
                 <div className="absolute inset-0 bottom-0 left-0 right-0 top-0 -z-50 inline-block bg-[url(/static/images/bg-grid.svg)] bg-center [mask-image:linear-gradient(180deg,black,rgba(0,0,0,0))]"></div>
 
                 {/* background profile image */}
-                <div className="absolute inset-0 bottom-0 left-auto right-0 top-auto -z-50 h-[40%] w-full bg-[url(/static/images/profile.png)] bg-contain bg-bottom bg-no-repeat [mask-image:linear-gradient(180deg,black,rgba(0,0,0,0))] md:h-full md:w-1/2"></div>
+                <div className="absolute inset-0 bottom-0 left-auto right-0 top-auto -z-50 h-[35%] w-full bg-[url(/static/images/profile.png)] bg-contain bg-bottom bg-no-repeat [mask-image:linear-gradient(180deg,black,rgba(0,0,0,0))] sm:h-[45%] md:h-full md:w-1/2"></div>
             </div>
         </>
     )
