a058327bbde4bcbbe810d0ddfc9451a5dacab64b
Matteo Giardino <mat.gia.dev@gmail.com>
2023-06-11 10:48:49 +0200

    feat: add testimonial

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Image from './mdx/image'
+
 const Testimonials = [
     {
         name: 'Ryan Florence',
@@ -75,26 +77,33 @@ export default function TestimonialsSection() {
             {/* Testimonial expanded */}
             <div className="relative isolate overflow-hidden px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:max-w-4xl">
-                    <img
-                        className="mx-auto h-12"
-                        src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg"
-                        alt=""
-                    />
+                    <img className="mx-auto h-12" src="/static/images/testimonials/revenue-farm-blue.png" alt="" />
                     <figure className="mt-10">
                         <blockquote className="text-center text-xl font-semibold leading-8 text-accent-7 sm:text-2xl sm:leading-9">
                             <p>
-                                “In questa sezione dovrebbero esserci delle recensioni, ma ancora non ho avuto il tempo
-                                di chiederle. Lascio spazio alla tua immaginazione”
+                                “Ho avuto modo di conoscere persone{' '}
+                                <strong className="underline decoration-sky-400 underline-offset-4">
+                                    competenti sul lato tecnico{' '}
+                                </strong>
+                                e persone competenti sul lato commerciale e di crescita. Poche persone comprendono in
+                                profondità tutti e due i temi e sanno portare valore ovunque serva a livello sia pratico
+                                che strategico,{' '}
+                                <strong className="underline decoration-sky-400 underline-offset-4">
+                                    Matteo è uno di questi unicorni{' '}
+                                </strong>
+                                ”
                             </p>
                         </blockquote>
                         <figcaption className="mt-10">
-                            <img
+                            <Image
                                 className="mx-auto h-10 w-10 rounded-full"
-                                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
+                                src="/static/images/testimonials/pietro-campanella.jpeg"
+                                width={40}
+                                height={40}
+                                alt="Pietro campanella"
                             />
                             <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                                <div className="font-semibold ">Judith Black</div>
+                                <div className="font-semibold ">Pietro Campanella</div>
                                 <svg
                                     viewBox="0 0 2 2"
                                     width={3}
@@ -103,7 +112,7 @@ export default function TestimonialsSection() {
                                     className="fill-gray-300">
                                     <circle cx={1} cy={1} r={1} />
                                 </svg>
-                                <div className="text-gray-300">CEO of Workcation</div>
+                                <div className="text-gray-300">CEO @ Revenue Farm</div>
                             </div>
                         </figcaption>
                     </figure>
